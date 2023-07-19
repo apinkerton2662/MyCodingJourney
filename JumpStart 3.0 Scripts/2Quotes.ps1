f7ddcff6e11bd9c6de439eb8f32c939a17400ed5
kaffieneaddikt <alex.s.pinkerton@outlook.com>
2023-07-18 16:41:41 -1000

    Moving files around

---
+++ JumpStart 3.0 Scripts/2Quotes.ps1
@@ -0,0 +1,12 @@
+﻿#Quotation Markes
+
+#Double quotes resolve the variable
+$i = "Powershell"
+
+"This is the variable $i, and $i Rocks!"
+'This is the variable $i, and $i Rocks!'
+"This is the variable `$i, and $i Rocks!"
+
+$computername = "Client"
+Get-Service -name bits -ComputerName "$ComputerName" | 
+    Select MachineName, Name, Status
---
+++ JumpStart 3.0 Scripts/3Params.ps1
@@ -0,0 +1,9 @@
+﻿# add the Param block
+
+#[CmdletBinding()]
+param(
+    [String]$ComputerName = 'Client',
+    [String]$Drive = 'c:'
+)
+Get-WmiObject -class Win32_logicalDisk -Filter "DeviceID = '$Drive'" -ComputerName $ComputerName
+
